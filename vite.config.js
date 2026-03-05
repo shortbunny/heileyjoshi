2734948fa0416ab5bf288199dc1bd179e5a855d2
Heiley Joshi <hjos0999@uni.sydney.edu.au>
2026-03-05 11:03:02 +1100

    changed according to domain name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/heileyjoshi',
+  base: '/',
   plugins: [react()],
   server: {
     historyApiFallback: true
